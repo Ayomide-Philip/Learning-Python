cdb77341702d57d67497111f04da94838e784cea
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-21 00:02:53 +0100

    fix: correct pizza size price calculation and improve input handling

--- DAY 3/pizzaOrderPractice.py
+++ DAY 3/pizzaOrderPractice.py
@@ -1,4 +1,16 @@
 print("Welcome to Pizza Deliveries!!")
-size = input("What size do you want? S, M or L:\n")
-pepperoni = input("Do you want Pepperoni on your pizza? Y or N:\n")
-extra_cheese = input("Do you want Extra cheese? Y or N:\n")
+size = input("What size do you want? S, M or L:\n").upper()
+pepperoni = input("Do you want Pepperoni on your pizza? Y or N:\n").upper()
+extra_cheese = input("Do you want Extra cheese? Y or N:\n").upper()
+
+price = 0
+
+# price based on the size they pick
+if size == "S":
+    price += 15
+elif size == "M":
+    price += 20
+elif price == "L":
+    price += 30
+
+print(price)
