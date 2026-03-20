9705fa0bdb99a2aeb18391311856329442f59795
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-20 23:59:09 +0100

    add pizzaOrderPractice.py for pizza order input prompts

---
+++ DAY 3/pizzaOrderPractice.py
@@ -0,0 +1,4 @@
+print("Welcome to Pizza Deliveries!!")
+size = input("What size do you want? S, M or L:\n")
+pepperoni = input("Do you want Pepperoni on your pizza? Y or N:\n")
+extra_cheese = input("Do you want Extra cheese? Y or N:\n")
