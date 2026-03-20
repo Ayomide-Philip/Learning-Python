643b7efd2fd090ce683696722a27b06e3bfd92cc
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-20 23:25:08 +0100

    add isEvenorOdd.py to check if a number is even or odd

---
+++ DAY 3/isEvenorOdd.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number to check if it is even or odd: \n"))
+
+if number % 2 == 0:
+    print(f"The number {number} is an Even Number")
+else:
+    print(f"The number {number} is an Odd Number")
