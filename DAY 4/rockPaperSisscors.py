69ff5ab7b6942621bf2c0b4bfde2a8465f66e4c7
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-22 13:19:15 +0100

    rock Sisscors paper game

--- DAY 4/rockPaperSisscors.py
+++ DAY 4/rockPaperSisscors.py
@@ -1,3 +1,25 @@
-print("What do you choose? Type 0 for Rock , 1 for Paper, 2 for Scissors \n")
+import random
+
+userInput = int(input("Type 0 for Rock , 1 for Paper, 2 for Scissors \n"))
 
 shape = ["Rock", "Paper", "Scissors"]
+
+randomGuess = int(random.random() * len(shape))
+
+randomShape = shape[randomGuess]
+
+if shape[userInput] == "Rock" and randomShape == "Scissors":
+    print(
+        f"""You picked {shape[userInput]} while Computer Picked {randomShape}.
+          You Won."""
+    )
+elif randomShape == "Rock" and shape[userInput] == "Scissors":
+    print(
+        f"""You picked {shape[userInput]} while Computer Picked {randomShape}.
+           Computer Won."""
+    )
+elif shape[userInput] == "Scissors" and randomShape == "Paper":
+    print(
+        f"""You picked {shape[userInput]} while Computer picked {randomShape}.
+          You Won."""
+    )
