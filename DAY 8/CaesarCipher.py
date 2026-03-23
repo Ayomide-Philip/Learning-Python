5e21033e10134192d2c9fc22ce0d134594a15167
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-23 16:45:08 +0100

    add user input prompts for Caesar cipher functionality

--- DAY 8/CaesarCipher.py
+++ DAY 8/CaesarCipher.py
@@ -26,3 +26,7 @@ alphabet = [
     "y",
     "z",
 ]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
