da0759e22bd6f8cb2e101dc8a90e424130bdc572
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-30 13:25:00 +0100

    updating my blackjade functionality

--- DAY 11/blackjade.py
+++ DAY 11/blackjade.py
@@ -2,67 +2,46 @@ import module
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-userCards = module.generateTwoRandomCard(cards)
-dealerCards = module.generateTwoRandomCard(cards)
-print(userCards)
-print(dealerCards)
-
-if userCards.count(10) != 0 and userCards.count(11) != 0:
-    if dealerCards.count(10) != 0 and dealerCards.count(11) != 0:
-        print("It's a draw")
-    else:
-        print("User Wins")
-elif dealerCards.count(10) != 0 and dealerCards.count(11) != 0:
-    print("Dealer Wins")
-
-
-def letUserDrawAnotherCardOrNot():
-    newCard = input("Enter 'y' to draw another card, and 'n' to skip?:").lower()
-    if newCard == "y":
-        userCards.append(random.choice(cards))
-        return True
-    elif newCard == "n":
-        return False
-    else:
-        return False
-
-
-userHasNewCard = True
-while userHasNewCard:
-    totalUserScore = module.calculateTotalCards(userCards)
-    totalDealerScore = module.calculateTotalCards(dealerCards)
-    print(f"Total User Score: {totalUserScore}")
-    print(f"Dealer Total Score:{totalDealerScore}")
-    if totalUserScore > 21:
-        if userCards.count(11) != 0:
-            totalUserAceScore = module.calculateTotalCardIfAce(userCards)
-            if totalUserAceScore > 21:
-                print(f"User Cards: {userCards}")
-                print(f"Dealer Cards:{dealerCards}")
-                print("User Busts, Dealer Wins")
-                userHasNewCard = not userHasNewCard
+doYouWantToPlayBlackJade = True
+while doYouWantToPlayBlackJade:
+    playBlackJack = input("Do you want to play a game of Blackjack? Type 'y' or 'n':")
+    if playBlackJack == "y":
+        userCards = module.generateTwoRandomCard(cards)
+        computerCard = module.generateTwoRandomCard(cards)
+        print(
+            f"Your Current Card {userCards}, current total {module.calculateTotalCards(userCards)}"
+        )
+        print(f"Computer first card:{computerCard[0]}")
+        if userCards.count(10) > 0 and userCards.count(11) > 0:
+            if computerCard.count(10) == 0 or computerCard.count(11) == 0:
+                print("User Won")
             else:
-                userHasNewCard = letUserDrawAnotherCardOrNot()
+                print("It's a Draw")
+        elif computerCard.count(10) > 0 and computerCard.count(11) > 0:
+            if userCards.count(10) == 0 or userCards.count(11) == 0:
+                print("Computer Wins")
+            else:
+                print("Its a draw")
         else:
-            print("User Burst, Dealer Wins")
-            userHasNewCard = False
+            userNewCards = True
+            while userNewCards:
+                if module.calculateTotalCards(userCards) > 21:
+                    print("User Card Greater than 21")
+                    if userCards.count(11) > 0:
+                        print("There is an Ace in user Card")
+                        totalUserAceCard = module.calculateTotalCardIfAce(userCards)
+                        if totalUserAceCard > 21:
+                            print("Computer Wins")
+                            userNewCards = False
+                        else:
+                            print("Loop through the who code again")
+                    else:
+                        print("User Has No Ace")
+                        print("Computer wins")
+                        userNewCards = False
+                else:
+                    print(False)
+                    userNewCards = False
     else:
-        userHasNewCard = letUserDrawAnotherCardOrNot()
-
-dealerHasNewCard = True
-while dealerHasNewCard:
-    print(dealerCards)
-    totalDealerScore = module.calculateTotalCards(dealerCards)
-    if totalDealerScore < 17:
-        dealerCards.append(random.choice(cards))
-
-    if totalDealerScore > 21 and dealerCards.count(11) != 0:
-        totalDealerAceScore = module.calculateTotalCardIfAce(dealerCards)
-        if totalDealerAceScore < 17:
-            dealerCards.append(random.choice(cards))
-        else:
-            dealerHasNewCard = False
-    else:
-        dealerHasNewCard = False
-
+        doYouWantToPlayBlackJade = not doYouWantToPlayBlackJade
+        print("Quitting!")
