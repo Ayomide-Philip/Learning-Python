167db7d8a2dc40317210315b989a58179c4d4391
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-30 01:01:19 +0100

    refactor blackjade.py and module.py to improve game logic and code clarity

--- DAY 11/blackjade.py
+++ DAY 11/blackjade.py
@@ -1,4 +1,3 @@
-import random
 import module
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -7,3 +6,11 @@ userCards = module.generateTwoRandomCard(cards)
 dealerCards = module.generateTwoRandomCard(cards)
 print(userCards)
 print(dealerCards)
+
+if userCards.count(10) != 0 and userCards.count(11) != 0:
+    if dealerCards.count(10) != 0 and dealerCards.count(11) != 0:
+        print("It's a draw")
+    else:
+        print("User Wins")
+elif dealerCards.count(10) != 0 and dealerCards.count(11) != 0:
+    print("Dealer Wins")
--- DAY 11/module.py
+++ DAY 11/module.py
@@ -3,6 +3,13 @@ import random
 
 def generateTwoRandomCard(cards):
     randomCard = []
-    for card in range(2):
+    for _ in range(2):
         randomCard.append(random.choice(cards))
     return randomCard
+
+
+def calculateTotalCards(cards):
+    totalCard = 0
+    for card in cards:
+        totalCard += card
+    return totalCard
