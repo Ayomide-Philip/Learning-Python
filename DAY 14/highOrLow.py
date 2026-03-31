8abe7b17d721071d9edd5fbaceeb7080d475bad5
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-31 19:19:35 +0100

    add functionality to compare two questions and prompt user for input

--- DAY 14/highOrLow.py
+++ DAY 14/highOrLow.py
@@ -8,3 +8,13 @@ def generate_starting_two_question():
 
 
 print(generate_starting_two_question())
+startingQuestionData = generate_starting_two_question()
+print(
+    f"""
+Compare A: {startingQuestionData[0]["name"]}, a {startingQuestionData[0]["description"]}, from {startingQuestionData[0]["country"]}.
+vs
+Against B: {startingQuestionData[1]["name"]}, a {startingQuestionData[1]["description"]}, from {startingQuestionData[1]["country"]}.
+"""
+)
+
+userInput = input("Who has more followers? Type 'A' or 'B':")
