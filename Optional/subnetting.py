8fe1584905b99d4405914b702f2da7fcda989519
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-01 19:15:02 +0100

    updating my codebase

--- Optional/subnetting.py
+++ Optional/subnetting.py
@@ -1,14 +1,14 @@
-import module
+# import module
 
 # user_ipv4_input = input(
-#     "Input An Ipv4 Address which you want to get the range of it's IP Address?e.g 192.168.0.1/24:\n"
+#     "Input An Ipv4 Address which you want to get the range of its IP Address?e.g 192.168.0.1/24:\n"
 # )
 
 
 def generateSubnetMaskOfTheIpAddress(subnet):
     if int(subnet) > 32:
         print("Subnet of an address cant be more than 32 bit")
-        return
+        return None
     binary_code_of_subnet = ""
     for i in range(1, 33):
         if i <= int(subnet):
@@ -17,9 +17,11 @@ def generateSubnetMaskOfTheIpAddress(subnet):
                 binary_code_of_subnet += "."
         else:
             binary_code_of_subnet += "0"
-    print(binary_code_of_subnet)
+            if i % 8 == 0 and i != 32:
+                binary_code_of_subnet += "."
+    return binary_code_of_subnet
 
 
 # print(module.isValidIpv4Address(user_ipv4_input))
 # print(module.generateBinaryNumberOfTheIpAddress(user_ipv4_input.split("/")[0]))
-generateSubnetMaskOfTheIpAddress(31)
+print(generateSubnetMaskOfTheIpAddress(10))
