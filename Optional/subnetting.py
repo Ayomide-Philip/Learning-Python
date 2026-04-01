8358bd43fb9567ef86efa6ae2d5894c74ed9e8bc
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-01 17:43:20 +0100

    updating subneting codebase

--- Optional/subnetting.py
+++ Optional/subnetting.py
@@ -1,2 +1,15 @@
+import ipaddress
+
 user_ipv4_input = input("Input An Ipv4 Address?")
-print(user_ipv4_input)
+
+
+def isValidIpv4Address(address):
+    """This function would check if the inputted ip address is an ipv4"""
+    try:
+        ipaddress.IPv4Address(address)
+        print(f"{address} its an ipv4 Address")
+    except ipaddress.AddressValueError:
+        print(f"{address} is not a valid ipv4 Address")
+
+
+isValidIpv4Address(user_ipv4_input)
