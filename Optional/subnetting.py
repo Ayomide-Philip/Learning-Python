7494ceb86f655cb1eb215ff8dec823c560439091
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-01 19:51:31 +0100

    updating my codebase

--- Optional/subnetting.py
+++ Optional/subnetting.py
@@ -5,8 +5,28 @@ import module
 # )
 
 
-
-
 # print(module.isValidIpv4Address(user_ipv4_input))
 # print(module.generateBinaryNumberOfTheIpAddress(user_ipv4_input.split("/")[0]))
-print(module.generateSubnetMaskOfTheIpAddress(24))
+# print(module.generateSubnetMaskOfTheIpAddress(24))
+
+
+def convertBinaryToReadableIpFormat(binary=""):
+    """This function convert binary back to ip address, and it also validates the new ip format generated to check if
+    it's a valid ip address by calling the function isIpAddressValid
+    """
+    binaryList = binary.split(".")
+    if len(binaryList) > 4:
+        print("An ipv4 Address cant have more than 3 dots")
+        return None
+    ipAddressList = []
+    for binary in binaryList:
+        binaryToInt = int(binary, 2)
+        ipAddressList.append(str(binaryToInt))
+    ipAddrInStr = ".".join(ipAddressList)
+    if module.isValidIpv4Address(ipAddrInStr):
+        return ipAddrInStr
+    else:
+        return None
+
+
+print(convertBinaryToReadableIpFormat(module.generateSubnetMaskOfTheIpAddress(24)))
