6256861827029eb30e2f131fbb3ea20d20195cbe
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-04-02 12:11:03 +0100

    updating my codebase

--- Optional/subnetting.py
+++ Optional/subnetting.py
@@ -1,7 +1,4 @@
 import module
-from module import totalNumberOfSubnetInANetwork
-from module import generateBinaryNumberOfTheIpAddress
-from module import generateSubnetMaskOfTheIpAddress
 
 
 # user_ipv4_input = input(
@@ -17,11 +14,39 @@ from module import generateSubnetMaskOfTheIpAddress
 # print(module.convertBinaryToReadableIpFormat(module.generateSubnetMaskOfTheIpAddress(24)))
 
 
-print(module.totalNumberOfHostInANetwork("26"))
+# print(module.totalNumberOfHostInANetwork("26"))
 
 
 # print(
 #     module.totalNumberOfSubnetInANetwork(
-#         generateBinaryNumberOfTheIpAddress("255.255.255.252")["ipv4_list"]
+#         module.generateBinaryNumberOfTheIpAddress("255.255.255.252")["ipv4_list"]
 #     )
 # )
+
+
+def getNetworkClass(ipAddress):
+    """This function is used to get the class an ip address, which range from 'A','B','C','D' and 'E', it uses a function
+    isValidIp44Address to check whether the ip address inputted is a valid ip, then returns the correct class if its valid
+    """
+    if not module.isValidIpv4Address(ipAddress):
+        return None
+
+    first_part_of_ip_address = ipAddress.split(".")[0]
+    ipClass = ""
+    if 0 <= int(first_part_of_ip_address) <= 127:
+        ipClass = "A"
+    elif 128 <= int(first_part_of_ip_address) <= 191:
+        ipClass = "B"
+    elif 192 <= int(first_part_of_ip_address) <= 223:
+        ipClass = "C"
+    elif 224 <= int(first_part_of_ip_address) <= 239:
+        ipClass = "D"
+    elif 240 <= int(first_part_of_ip_address) <= 255:
+        ipClass = "E"
+    else:
+        return None
+
+    return {"class": ipClass}
+
+
+print(getNetworkClass("192.168.0.1"))
